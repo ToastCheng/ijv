83dccbb693ce6c9b1d20959902833f03919f8473
ToastCheng <MrToastCheng@gmail.com>
2018-12-10 15:26:58 +0800

    trained one

--- inverse/model.py
+++ inverse/model.py
@@ -2,8 +2,71 @@ import torch
 import torch.nn as nn
 
 
-
 class SCVNet(nn.Module):
+	def __init__(self, spectrum_size=36, geometry_size=6, parameter_size=23):
+		super(SCVNet, self).__init__()
+
+		self.encoder_geometry = nn.Sequential(
+			nn.Linear(geometry_size, 50),
+			nn.BatchNorm1d(50),
+			nn.ReLU(),
+			nn.Linear(50, 50)
+		)
+
+		self.encoder_spectrum = nn.Sequential(
+			nn.Linear(spectrum_size, 50),
+			nn.BatchNorm1d(50),
+			nn.ReLU(),
+			nn.Linear(50, 50)
+		)
+
+		self.encoder_parameter = nn.Sequential(
+			nn.Linear(parameter_size, 50),
+			nn.BatchNorm1d(50),
+			nn.ReLU(),
+			nn.Linear(50, 50)
+		)
+
+		self.fc = nn.Sequential(
+			nn.Linear(150, 300),
+			nn.BatchNorm1d(300),
+			nn.ReLU(),
+			nn.Linear(300, 300),
+			nn.BatchNorm1d(300),
+			nn.ReLU(), 
+			nn.Linear(300, 1)
+		)
+
+
+
+	def forward(self, spectrum, geometry, parameter):
+		# spectrum: [batch, spectrum_size]
+		# geometry: [batch, geometry_size]
+
+		# encode the geometry
+
+		# [batch, 50]
+		geometry = self.encoder_geometry(geometry)
+
+		# [batch, 50]
+		parameter = self.encoder_parameter(parameter)
+
+		# spectrum flatten
+		spectrum = spectrum.view(spectrum.shape[0], -1)
+
+		# [batch, 50]
+		spectrum = self.encoder_spectrum(spectrum)
+
+		# [batch, 100]
+		processed_input = torch.cat([spectrum, geometry, parameter], 1)
+
+		out = self.fc(processed_input)
+
+		return out
+
+
+
+class SCVNetV1(nn.Module):
 	def __init__(self, spectrum_size=36, geometry_size=6, parameter_size=23):
 		super(SCVNet, self).__init__()
 
--- train.py
+++ train.py
@@ -5,6 +5,9 @@ import torch
 import torch.nn as nn 
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+
+import numpy as np 
 import matplotlib.pyplot as plt
 
 from inverse.model import SCVNet
@@ -12,12 +15,13 @@ from inverse.dataset import SimData
 
 
 
+
 def get_args():
 	parser = ArgumentParser()
 
 	parser.add_argument("-l", "--learning_rate", type=float, default=1e-4)
-	parser.add_argument("-e", "--epoch", type=int, default=100)
-	parser.add_argument("-p", "--pretrain", type=int, default=0)
+	parser.add_argument("-e", "--epoch", type=int, default=300)
+	parser.add_argument("-p", "--pretrain", action="store_true")
 	parser.add_argument("-m", "--model", type=str, default="")
 	parser.add_argument("-b", "--batch_size", type=int, default=32)
 
@@ -29,18 +33,40 @@ def train():
 
 	net = SCVNet()
 
-	net.load_state_dict(torch.load("saved_model/model.pth"))
+	if args.pretrain:
+		net.load_state_dict(torch.load("saved_model/model.pth"))
 
 	dataset = SimData()
-	loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+
+	# train test split
+	dataset_size = len(dataset)
+	indices = list(range(dataset_size))
+	split = int(np.floor(0.1 * dataset_size))
+
+	# shuffle
+	# np.random.seed(random_seed)
+	np.random.shuffle(indices)
+	
+	train_indices, val_indices = indices[split:], indices[:split]
+
+	train_sampler = SubsetRandomSampler(train_indices)
+	valid_sampler = SubsetRandomSampler(val_indices)
+
+	# if sampler is used, do not set "shuffle"
+	# drop last since there might be only 1 data in the last batch
+	train_loader = DataLoader(dataset, batch_size=args.batch_size, sampler=train_sampler, drop_last=True)
+	valid_loader = DataLoader(dataset, batch_size=split, sampler=valid_sampler)
+
+
 
 	loss_func = nn.MSELoss()
 	optimizer = optim.Adam(params=net.parameters(), lr=args.learning_rate)
 
-	losses = []
+	train_losses = []
+	valid_losses = []
 
 	for e in range(args.epoch):
-		for idx, data in enumerate(loader):
+		for idx, data in enumerate(train_loader):
 
 			spectrum, geometry, parameter = data[0]
 			scv = data[1]
@@ -62,17 +88,34 @@ def train():
 			loss.backward()
 			optimizer.step()
 
-		losses.append(loss)
+		for data in valid_loader:
+			s, g, p = data[0]
+			_scv = data[1]
+
+			s = s.float()
+			g = g.float()
+			p = p.float()
+			_scv = _scv.float()
+
+			_pred = net(s, g, p)
+
+			_loss = loss_func(_pred, _scv)
+
+		train_losses.append(loss)
+		valid_losses.append(_loss)
 
 		if e%5 == 0:
-			print("epoch: %d | loss: %f.4" %(e, loss))
+			print("================="*2)
+			print("epoch: %d | train loss: %.4f | valid loss: %.4f" %(e, loss, _loss))
 			print("predict: ", pred[:5])
 			print("target: ", scv[:5])
 
 
-	plt.plot(losses)
+	plt.plot(train_losses, label="train")
+	plt.plot(valid_losses, label="valid")
 	plt.xlabel('epoch')
 	plt.ylabel('rms error')
+	plt.legend()
 	plt.savefig('log.png')
 
 	torch.save(net.state_dict(), "saved_model/model.pth")
