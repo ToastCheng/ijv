bd040993b16ed17a7a86bcbb44272d6c23d4555c
ToastC <mrtoastcheng@gmail.com>
2019-05-13 19:12:33 +0800

    ..

---
+++ copy_gcp.sh
@@ -0,0 +1,4 @@
+scp -r mrtoastcheng@35.194.251.128:~/ijv-1.0/train/spec train/
+scp -r mrtoastcheng@104.199.165.27:~/ijv-1.0/train/spec train/
+scp -r mrtoastcheng@35.221.184.51:~/ijv-1.0/train/spec train/
+scp -r mrtoastcheng@34.80.79.30:~/ijv-1.0/train/spec train/
