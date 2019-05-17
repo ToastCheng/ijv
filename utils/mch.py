705a24d4bd478affc5bc065e6a40ab113beaa2a9
chiaoyiwang <r05945007@ntu.edu.tw>
2019-05-17 16:15:22 +0800

    speedup wmc

--- utils/mch.py
+++ utils/mch.py
@@ -9,8 +9,8 @@ import os
 
 from utils import load_mch
 
-# device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-device = torch.device('cpu')
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+# device = torch.device('cpu')
 
 class MCHHandler:
     # This class is a .mch file handler.
@@ -63,119 +63,7 @@ class MCHHandler:
         self.critical_angle = np.arcsin(self.detector_na/self.detector_n)     
 
 
-    def run_wmc_dev(self, args=None):
-
-        if not self.config:
-            raise Exception("Should specify the config file first!")
-
-        spectra = []
-        portions = []
-
-
-        for f in self.mch_list:
-            if self.config["type"] == "ijv":
-                wl = int(f.split('_')[-1].strip('.mch'))
-            elif self.config["type"] == "muscle":
-                wl = int(f.split('_')[-1].strip('.mch'))
-            elif self.config["type"] == "phantom":
-                wl = int(f.split('_')[-2])
-            else:
-                raise Exception("Tissue type error!")
-
-            df, header, photon = self._load_mch(f)
-
-            df = df[np.arccos(df.angle.abs()) <= self.critical_angle]
-            if len(df) == 0:
-                print('no photon detected at %d' % wl)
-                return None, None
-            df = df.reset_index(drop=True)
-
-            # [photon, medium]
-            # 0: detector index
-            # 1: prism
-            path_length = df.iloc[:, 2:-1].values
-
-            path_length = torch.tensor(path_length).float().to(device)
-
-            # [medium, ScvO2]
-            if self.config["type"] != "phantom":
-                mua = self._make_tissue_white(wl, header, args)
-            else:
-                phantom_id = f.split('_')[-1].strip('.mch')
-                mua = self._make_tissue_phantom(wl, header, phantom_id)
-
-            mua = torch.tensor(mua).float().to(device)
-
-            # [photon, ScvO2]
-            weight = torch.exp(-torch.matmul(path_length, mua) *header["unitmm"]) 
-            
-            # NEW 
-            # df["weight"] = weight.tolist()
-            # 3 計算實際路徑長*權重 (in 95%)
-            # print(path_length.shape)
-            # print(weight[:, 96].shape)
-            path_length = (path_length * weight[:].unsqueeze(1))
-            if self.config["type"] == "ijv":
-                skin_portion = (path_length.mean(0)[0] ).tolist()
-                fat_portion = (path_length.mean(0)[1]).tolist()
-                muscle_portion = (path_length.mean(0)[2]).tolist()
-                ijv_portion = (path_length.mean(0)[3]).tolist()
-                cca_portion = (path_length.mean(0)[4]).tolist()
-
-            elif self.config["type"] == "muscle":
-                skin_portion = (path_length.mean(0)[0] ).tolist()
-                fat_portion = (path_length.mean(0)[1]).tolist()
-                muscle_portion = (path_length.mean(0)[2]).tolist()
-
-            elif self.config["type"] == "phantom":
-                phantom_portion = (path_length.mean(0)[0] ).tolist()
-
-            # [1, ScvO2]
-            result = torch.zeros(1).float().to(device)
-
-            # seperate photon with different detector
-            for idx in range(1, header["detnum"]+1):
-
-                # get the index of specific index
-                detector_list = df.index[df["detector_idx"] == idx].tolist()
-                if len(detector_list) == 0:
-                    # print("detector #%d detected no photon" % idx)
-                    result = torch.cat((result, torch.zeros(1, weight.shape[1]).float().to(device)), 0)
-                    continue
-                # pick the photon that detected by specific detector
-                
-                # [photon, ScvO2]
-                _weight = weight[detector_list]
-                # print(_weight.shape)
-                _weight = _weight.sum(0)
-                _weight = _weight.unsqueeze(0)
-                # print(_weight.shape)
-                # print(result.shape)
-                result = torch.cat((result, _weight), 0)
-
-            # [SDS, ScvO2]
-            result = result[1:]
-            s = result.cpu().numpy()/header["total_photon"]
-            
-            spectra.append(s)
-
-            if self.config["type"] == "ijv":
-                portions.append(
-                    [skin_portion, fat_portion, muscle_portion, ijv_portion, cca_portion]
-                )
-            elif self.config["type"] == "muscle":
-                portions.append(
-                    [skin_portion, fat_portion, muscle_portion]
-                )
-            elif self.config["type"] == "phantom":
-                portions.append([phantom_portion])
-            else:
-                raise Exception("tissue type does not supported yet")
-
-        return np.asarray(spectra).T, np.asarray(portions).T
-            
-
-            
+        
     def _make_tissue_phantom(self, wl, header, phantom_id):
 
         phantom = self.mua[phantom_id].values
@@ -191,8 +79,6 @@ class MCHHandler:
 
     def _make_tissue_white(self, wl, header, args):
         
-        if isinstance(dict, args):
-            args = [args]
         # mua
         oxy = self.mua['oxy'].values
         deoxy = self.mua['deoxy'].values
@@ -224,11 +110,11 @@ class MCHHandler:
         _mua = []
         for arg in args:
             skin = self._calculate_mua(
-                args["skin"]["blood_volume_fraction"],
-                args["skin"]["ScvO2"],
-                args["skin"]["water_volume"],
-                args["skin"]["fat_volume"],
-                args["skin"]["melanin_volume"],
+                arg["skin"]["blood_volume_fraction"],
+                arg["skin"]["ScvO2"],
+                arg["skin"]["water_volume"],
+                arg["skin"]["fat_volume"],
+                arg["skin"]["melanin_volume"],
                 oxy, 
                 deoxy, 
                 water,
@@ -241,7 +127,7 @@ class MCHHandler:
                 0,
                 0,
                 0,
-                args["fat"]["fat_volume"],
+                arg["fat"]["fat_volume"],
                 0,
                 oxy, 
                 deoxy, 
@@ -252,11 +138,11 @@ class MCHHandler:
                 )
 
             muscle = self._calculate_mua(
-                args["muscle"]["blood_volume_fraction"],
-                args["muscle"]["ScvO2"],
-                args["muscle"]["water_volume"],
-                args["muscle"]["fat_volume"],
-                args["muscle"]["melanin_volume"],
+                arg["muscle"]["blood_volume_fraction"],
+                arg["muscle"]["ScvO2"],
+                arg["muscle"]["water_volume"],
+                arg["muscle"]["fat_volume"],
+                arg["muscle"]["melanin_volume"],
                 oxy,
                 deoxy,
                 water,
@@ -267,11 +153,11 @@ class MCHHandler:
 
             if self.config["type"] == "ijv":
                 IJV = self._calculate_mua(
-                    args["ijv"]["blood_volume_fraction"],
-                    args["ijv"]["ScvO2"],
-                    args["ijv"]["water_volume"],
-                    args["ijv"]["fat_volume"],
-                    args["ijv"]["melanin_volume"],
+                    arg["ijv"]["blood_volume_fraction"],
+                    arg["ijv"]["ScvO2"],
+                    arg["ijv"]["water_volume"],
+                    arg["ijv"]["fat_volume"],
+                    arg["ijv"]["melanin_volume"],
                     oxy, 
                     deoxy, 
                     water,
@@ -280,11 +166,11 @@ class MCHHandler:
                     collagen
                     )
                 CCA = self._calculate_mua(
-                    args["cca"]["blood_volume_fraction"],
-                    args["cca"]["ScvO2"],
-                    args["cca"]["water_volume"],
-                    args["cca"]["fat_volume"],
-                    args["cca"]["melanin_volume"],
+                    arg["cca"]["blood_volume_fraction"],
+                    arg["cca"]["ScvO2"],
+                    arg["cca"]["water_volume"],
+                    arg["cca"]["fat_volume"],
+                    arg["cca"]["melanin_volume"],
                     oxy, 
                     deoxy, 
                     water,
@@ -313,7 +199,7 @@ class MCHHandler:
             else:
                 raise Exception("tissue type does not supported yet")
                 
-        return np.asarray(_mua)
+        return np.asarray(_mua).T
 
 
     @staticmethod
@@ -344,6 +230,9 @@ class MCHHandler:
         if not self.config:
             raise Exception("Should specify the config file first!")
 
+        if isinstance(args, dict):
+            args = [args]
+            
         spectra = []
         portions = []
 
@@ -366,14 +255,14 @@ class MCHHandler:
                 return None, None
             df = df.reset_index(drop=True)
 
-            # [photon, medium]
+            # [光子數, 組織數]
             # 0: detector index
             # 1: prism
             path_length = df.iloc[:, 2:-1].values
 
             path_length = torch.tensor(path_length).float().to(device)
 
-            # [medium, ScvO2]
+            # [ 組織數, 吸收數]
             if self.config["type"] != "phantom":
                 mua = self._make_tissue_white(wl, header, args)
             else:
@@ -382,21 +271,22 @@ class MCHHandler:
 
             mua = torch.tensor(mua).float().to(device)
 
-            # [photon, ScvO2]
-            weight = torch.exp(-torch.matmul(path_length, mua) *header["unitmm"]) 
             
+            # [光子數, 吸收數]
+            weight = torch.exp(-torch.matmul(path_length, mua) *header["unitmm"]) 
+
+
             # NEW 
             # df["weight"] = weight.tolist()
             # 3 計算實際路徑長*權重 (in 95%)
-            # print(path_length.shape)
-            # print(weight[:, 96].shape)
-            path_length = (path_length * weight[:].unsqueeze(1))
+
+            path_length = (path_length.transpose(0, 1) @ weight)
             if self.config["type"] == "ijv":
-                skin_portion = (path_length.mean(0)[0] ).tolist()
-                fat_portion = (path_length.mean(0)[1]).tolist()
-                muscle_portion = (path_length.mean(0)[2]).tolist()
-                ijv_portion = (path_length.mean(0)[3]).tolist()
-                cca_portion = (path_length.mean(0)[4]).tolist()
+                skin_portion = (path_length[0] ).tolist()
+                fat_portion = (path_length[1]).tolist()
+                muscle_portion = (path_length[2]).tolist()
+                ijv_portion = (path_length[3]).tolist()
+                cca_portion = (path_length[4]).tolist()
 
             elif self.config["type"] == "muscle":
                 skin_portion = (path_length.mean(0)[0] ).tolist()
@@ -405,9 +295,12 @@ class MCHHandler:
 
             elif self.config["type"] == "phantom":
                 phantom_portion = (path_length.mean(0)[0] ).tolist()
+            ###
+            ######
+            #########
 
-            # [1, ScvO2]
-            result = torch.zeros(1).float().to(device)
+            # [1, 吸收數]
+            result = torch.zeros(1, len(args)).float().to(device)
 
             # seperate photon with different detector
             for idx in range(1, header["detnum"]+1):
@@ -415,18 +308,17 @@ class MCHHandler:
                 # get the index of specific index
                 detector_list = df.index[df["detector_idx"] == idx].tolist()
                 if len(detector_list) == 0:
-                    # print("detector #%d detected no photon" % idx)
+                    # print("detector #%d detected 0 photon" % idx)
                     result = torch.cat((result, torch.zeros(1, weight.shape[1]).float().to(device)), 0)
                     continue
+                    
                 # pick the photon that detected by specific detector
                 
-                # [photon, ScvO2]
+                # [光子數, 吸收數]
                 _weight = weight[detector_list]
-                # print(_weight.shape)
                 _weight = _weight.sum(0)
                 _weight = _weight.unsqueeze(0)
-                # print(_weight.shape)
-                # print(result.shape)
+                
                 result = torch.cat((result, _weight), 0)
 
             # [SDS, ScvO2]
@@ -451,143 +343,8 @@ class MCHHandler:
         return np.asarray(spectra).T, np.asarray(portions).T
             
 
-            
-    def _make_tissue_phantom(self, wl, header, phantom_id):
-
-        phantom = self.mua[phantom_id].values
-        wavelength = self.mua['wl'].values
-
-        phantom = np.interp(wl, wavelength, phantom)
-
-        # if the mua is 1/cm, you need to change the unit into 1/mm
-        # mua *= 0.1
-
-        return np.expand_dims(phantom, 0)
 
 
-    def _make_tissue_white(self, wl, header, args):
-        
-        # mua
-        oxy = self.mua['oxy'].values
-        deoxy = self.mua['deoxy'].values
-        water = self.mua['water'].values
-        collagen = self.mua['collagen'].values
-        fat = self.mua['fat'].values
-        melanin = self.mua['mel'].values
-        wavelength = self.mua['wavelength'].values
-
-        # interpolation
-        oxy = np.interp(wl, wavelength, oxy)
-        deoxy = np.interp(wl, wavelength, deoxy)
-        water = np.interp(wl, wavelength, water)
-        collagen = np.interp(wl, wavelength, collagen)
-        fat = np.interp(wl, wavelength, fat)
-        melanin = np.interp(wl, wavelength, melanin)
-
-        # turn the unit 1/cm --> 1/mm
-        oxy *= 0.1
-        deoxy *= 0.1
-        water *= 0.1
-        collagen *= 0.1
-        fat *= 0.1
-        melanin *= 0.1
-
-        # [medium, 1]
-        # mua = np.zeros((header["maxmedia"], 1))
-
-
-
-        skin = self._calculate_mua(
-            args["skin"]["blood_volume_fraction"],
-            args["skin"]["ScvO2"],
-            args["skin"]["water_volume"],
-            args["skin"]["fat_volume"],
-            args["skin"]["melanin_volume"],
-            oxy, 
-            deoxy, 
-            water,
-            fat,
-            melanin,
-            collagen
-            )
-
-        fat = self._calculate_mua(
-            0,
-            0,
-            0,
-            args["fat"]["fat_volume"],
-            0,
-            oxy, 
-            deoxy, 
-            water,
-            fat,
-            melanin,
-            collagen
-            )
-
-        muscle = self._calculate_mua(
-            args["muscle"]["blood_volume_fraction"],
-            args["muscle"]["ScvO2"],
-            args["muscle"]["water_volume"],
-            args["muscle"]["fat_volume"],
-            args["muscle"]["melanin_volume"],
-            oxy,
-            deoxy,
-            water,
-            fat,
-            melanin,
-            collagen,
-            )
-
-        if self.config["type"] == "ijv":
-            IJV = self._calculate_mua(
-                args["ijv"]["blood_volume_fraction"],
-                args["ijv"]["ScvO2"],
-                args["ijv"]["water_volume"],
-                args["ijv"]["fat_volume"],
-                args["ijv"]["melanin_volume"],
-                oxy, 
-                deoxy, 
-                water,
-                fat,
-                melanin,
-                collagen
-                )
-            CCA = self._calculate_mua(
-                args["cca"]["blood_volume_fraction"],
-                args["cca"]["ScvO2"],
-                args["cca"]["water_volume"],
-                args["cca"]["fat_volume"],
-                args["cca"]["melanin_volume"],
-                oxy, 
-                deoxy, 
-                water,
-                fat,
-                melanin,
-                collagen
-                )
-
-        if self.config["type"] == "ijv":
-            _mua = np.concatenate(
-                [np.expand_dims(skin, 0),
-                np.expand_dims(fat, 0),
-                 np.expand_dims(muscle, 0), 
-                 np.expand_dims(IJV, 0), 
-                 np.expand_dims(CCA, 0)], 0
-                 )
-
-        elif self.config["type"] == "muscle":
-            _mua = np.concatenate(
-                [np.expand_dims(skin, 0),
-                 np.expand_dims(fat, 0),
-                 np.expand_dims(muscle, 0)
-                ], 0
-                 )
-
-        else:
-            raise Exception("tissue type does not supported yet")
-            
-        return _mua
 
 
     @staticmethod
@@ -600,9 +357,11 @@ class MCHHandler:
         elif data[1]["seed_byte"] == 1:
             df, header, photon_seed = data
 
+        
         num_media = header["maxmedia"] 
         # selected_list = [0] + [i for i in range(num_media+1, 2*num_media+1)] + [-1]
         selected_list = [0] + [i for i in range(2, 2 + num_media)] + [-1]
+        
         df = df[:, selected_list]
         label = ["detector_idx"]
         label += ["media_{}".format(i) for i in range(header["maxmedia"])]
