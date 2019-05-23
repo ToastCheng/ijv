48e488b2c2df0c8abf14b6b1578a29276d693cb6
toast <tushicheng@toastde-MacBook-Air.local>
2019-05-23 20:53:59 +0800

    修正每個組織的musp

--- run.py
+++ run.py
@@ -12,5 +12,5 @@ mcx = MCX()
 # from mcx_test import MCX
 from mcx_test import MCX
 mcx = MCX()
-mcx.run("configs/phantom_muscle.json")
-# mcx.run("configs/live/{}.json".format(sys.argv[1]))
+# mcx.run("configs/phantom_muscle.json")
+mcx.run("configs/live/{}.json".format(sys.argv[1]))
